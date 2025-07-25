a2c3481b781a7f584fcbb6253922e7168a020500
Buendilyn <buendilyn@gmail.com>
2025-07-25 10:25:24 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -5,5 +5,14 @@ use Dimapasok\Gs\Models\StudentModel;
 require 'vendor/autoload.php';
 
 $student = new StudentModel;
+
+$student->id = 2202;
+$student->name ="Buen";
+$student->course = "BSIT";
+$student->year_level = 1;
+$student->section ="D";
+
+
+$student->create();
 $listOfStudent = $student->read();
 print_r($listOfStudent);
