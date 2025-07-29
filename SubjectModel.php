bb3350cbcd0d6e26e59f6fd58d4057628907da9e
Buendilyn <buendilyn@gmail.com>
2025-07-29 14:45:12 +0800

    Add files via upload

---
+++ SubjectModel.php
@@ -0,0 +1,23 @@
+<?php
+namespace Dimapasok\Gs\Model;
+class SubjectModel {
+    public function create()
+    {
+
+    }
+    public function read()
+    {
+
+    }
+    public function update()
+    {
+
+    }
+    public function delete()
+    {
+
+    }
+    
+    
+
+}
